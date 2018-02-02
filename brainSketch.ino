0954dd045b34b67e080dc32b98eeef3f0876f75c
DueyDu <msdu1@asu.edu>
2018-02-02 16:44:59 -0700

    Update brainSketch.ino

--- brainSketch.ino
+++ brainSketch.ino
@@ -1,11 +1,6 @@
 #include <MPU9250.h>
 #include <quaternionFilters.h>
-/* MPU9250 Basic Example Code
- by: Kris Winer
- date: April 1, 2014
- license: Beerware - Use this code however you'd like. If you
- find it useful you can buy me a beer some time.
- Modified by Brent Wilkins July 19, 2016
+/* MPU9250 Code
 
  Demonstrate basic MPU-9250 functionality including parameterizing the register
  addresses, initializing the sensor, getting properly scaled accelerometer,
